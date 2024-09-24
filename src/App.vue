9b8448022ab8863521c7ef8a6d5c13afb459ca63
NikDm <respectedoff@yandex.ru>
2024-09-24 14:37:20 +0300

    views

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="wrapper">
-  <home-view></home-view>
+  <router-view></router-view>
 </div>
 
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,49 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
+import DagestanView from '@/views/DagestanView.vue'
+import TerebikeView from "@/views/TerebikeView.vue"
+import ElbrusView from "@/views/ElbrusView.vue"
+import CrimeaView from "@/views/CrimeaView.vue"
+import PolarView from "@/views/PolarView.vue"
+import AltaiView from "@/views/AltaiView.vue"
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/dagestan',
+    component: DagestanView
+  },
+  {
+    path: '/terebike',
+    component: TerebikeView
+  },
+  {
+    path: '/elbrus',
+    component: ElbrusView
+  },
+  {
+    path: '/crimea',
+    component: CrimeaView
+  },
+  {
+    path: '/polar',
+    component: PolarView
+  },
+  {
+    path: '/altai',
+    component: AltaiView
+  },
+
 ]
 
 const router = new VueRouter({
+  mode: 'history',
   routes
 })
 
